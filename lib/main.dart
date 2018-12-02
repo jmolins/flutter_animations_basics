c6725760964148efb9a9fc277127c1cc5fb3ee9b
Chema Molins <chemamolins@gmail.com>
2018-12-03 00:04:35 +0100

    Set first animation for AnimatedHome0

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: AnimatedHome7(),
+      home: AnimatedHome0(),
     );
   }
 }
